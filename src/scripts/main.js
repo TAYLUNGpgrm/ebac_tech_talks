3dfac7cf2386fda90eed7a47c3de75a48c956869
Alef Dutra <alefdutran@gmail.com>
2025-03-24 11:38:46 -0300

    contagem regressiva

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
-console.log('Hello, World!');
+AOS.init();
+
+const dataDoEvento = new Date("Dec 12, 2025 19:00:00");
+const timeStempDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function () {
+    const agora = new Date();
+    const timeStempAgora = agora.getTime();
+    const timeStempRestante = timeStempDoEvento - timeStempAgora;
+    
+    const segundos = Math.floor((timeStempRestante % (1000 * 60)) / 1000);
+    const minutos = Math.floor((timeStempRestante % (1000 * 60 * 60)) / (1000 * 60));
+    const horas = Math.floor((timeStempRestante % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const dias = Math.floor(timeStempRestante / (1000 * 60 * 60 * 24));
+
+    document.getElementById("contador").innerHTML = `${dias} dias ${horas} horas ${minutos} minutos ${segundos} segundos`;
+}, 1000)
